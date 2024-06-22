bebdd6692eced84001ef4d30923b403ec2a08824
DentistOmarAshraf <omar_ashraf@live.com>
2024-06-22 22:26:46 +0000

    ALL RELATION ARE setteled down TO build the Main DB

--- sqlrev.py
+++ sqlrev.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from sqlalchemy import Column, String, Integer, create_engine, ForeignKey
+from sqlalchemy import Column, String, Integer, create_engine, ForeignKey, Table
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 
@@ -11,6 +11,39 @@ session = Session()
 
 Base = declarative_base()
 
+
+class Service(Base):
+    __tablename__ = "service"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60))
+    clinic = relationship("Clinic", secondary="clinic_service",
+            back_populates="services")
+
+
+class Reservation(Base):
+    __tablename__ = "reservation"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60))
+    user_id = Column(Integer, ForeignKey("user.id"))
+    clinic_id = Column(Integer, ForeignKey("clinic.id"))
+    user = relationship("User", back_populates="reservation")
+    clinic = relationship("Clinic", back_populates="reservations")
+
+class Review(Base):
+    __tablename__ = "review"
+    id = Column(Integer, primary_key=True)
+    text = Column(String(1024))
+    clinic_id = Column(Integer, ForeignKey("clinic.id"))
+    user_id = Column(Integer, ForeignKey("user.id"))
+    clinic = relationship("Clinic", back_populates="reviews")
+    user = relationship("User", back_populates="reviews")
+
+
+clinic_service = Table("clinic_service", Base.metadata,
+        Column("serivce_id", Integer, ForeignKey("service.id"), primary_key=True),
+        Column("clinic_id", Integer, ForeignKey("clinic.id"), primary_key=True))
+
+
 class Clinic(Base):
     __tablename__ = "clinic"
     id = Column(Integer, primary_key=True)
@@ -19,6 +52,11 @@ class Clinic(Base):
     address_id = Column(Integer, ForeignKey("address.id"))
     user = relationship("User", back_populates="clinic")
     address = relationship("Address", cascade="all, delete")
+    reviews = relationship("Review", cascade="all, delete", back_populates="clinic")
+    reservations = relationship("Reservation", back_populates="clinic",
+                cascade="all, delete")
+    services = relationship("Service", secondary="clinic_service",
+            back_populates="clinic")
 
 class User(Base):
     __tablename__ = "user"
@@ -27,6 +65,9 @@ class User(Base):
     address_id = Column(Integer, ForeignKey("address.id"))
     address = relationship("Address", cascade="all, delete")
     clinic = relationship("Clinic", cascade="all, delete", back_populates="user")
+    reviews = relationship("Review", cascade="all, delete", back_populates="user")
+    reservation = relationship("Reservation", cascade="all, delete",
+            back_populates="user")
 
 class City(Base):
     __tablename__ = "city"
@@ -56,21 +97,44 @@ cairo = City(id=1, name="Cairo")
 place_1 = Neighborhood(id=1, name="Mokattam")
 mkan_1 = Address(id=1, name="Nafora")
 mkan_2 = Address(id=2, name="Nafora")
+mkan_3 = Address(id=3, name="st 81")
 
 cairo.neighborhoods.append(place_1)
 place_1.addresses.append(mkan_1)
 place_1.addresses.append(mkan_2)
+place_1.addresses.append(mkan_3)
 
 omar = User(id=1, name="omar")
+nada = User(id=2, name="nada")
 omar.address = mkan_1
+nada.address = mkan_3
 
 dental = Clinic(id=1, name="Fayroz")
 dental.user = omar
 dental.address = mkan_2
 
+review = Review(id=1, text="Good_clinic")
+review.user = omar
+review.clinic = dental
+
+new_res = Reservation(id=1, name="Endo")
+new_res.clinic = dental
+new_res.user = nada
+
+endo = Service(id=1, name="Endo")
+opt = Service(id=2, name="OPT")
+dental.services.append(endo)
+dental.services.append(opt)
+
 session.add(cairo)
 session.add(omar)
 session.add(dental)
-session.commit()
-session.delete(omar)
+session.add(review)
+session.add(new_res)
+session.add(endo)
+session.add(opt)
+print(new_res.user.name)
+print(new_res.clinic.name)
+print(dental.services[0].name)
+print(dental.services[1].name)
 session.commit()
