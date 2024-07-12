00c9417f8f156e6d5374a2ab462124cd70a7807a
DentistOmarAshraf <omar_ashraf@live.com>
2024-07-12 08:11:28 +0000

    scripts

--- run_api.sh
+++ run_api.sh
@@ -1,1 +1,1 @@
-CALEN_STORAGE_TYPE=db CALEN_USR=calen_dev CALEN_PWD=calen_dev_pwd CALEN_DB=calen_dev_db CALEN_HOST=localhost python3 -m api.v1.app
+CALEN_STORAGE_TYPE=db CALEN_USR=calen_dev CALEN_PWD=calen_dev_pwd CALEN_DB=calen_dev_db CALEN_HOST=localhost tmux new-session -d 'gunicorn --bind 0.0.0.0:5001 api.v1.app:app'
--- run_web.sh
+++ run_web.sh
@@ -1,1 +1,1 @@
-CALEN_STORAGE_TYPE=db CALEN_USR=calen_dev CALEN_PWD=calen_dev_pwd CALEN_DB=calen_dev_db CALEN_HOST=localhost python3 -m web.app
+CALEN_STORAGE_TYPE=db CALEN_USR=calen_dev CALEN_PWD=calen_dev_pwd CALEN_DB=calen_dev_db CALEN_HOST=localhost tmux new-session -d 'gunicorn --bind 0.0.0.0:5000 web.app:app'
